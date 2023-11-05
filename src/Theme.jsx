0aa61bb587d2b9bd32b15c143fba5a130091fc27
Dvir Bartov <dvir.bartov1@gmail.com>
2023-11-05 20:15:35 +0200

    Setup fonts and mui themes

--- src/Theme.jsx
+++ src/Theme.jsx
@@ -1,7 +1,59 @@
 import { createTheme } from "@mui/material";
 
-const theme = createTheme({
+let theme = createTheme({
+    palette: {
+        primary: {
+            main: '#C85FCE'
+        },
+        secondary: {
+            main: '#662A68'
+
+        }
+    },
     direction: "rtl"
+})
+
+theme = createTheme(theme, {
+    typography: {
+        fontFamily: [
+            'NotoSansHebrew',
+            '-apple-system',
+            'BlinkMacSystemFont',
+            '"Segoe UI"',
+            'Roboto',
+            '"Helvetica Neue"',
+            'Arial',
+            'sans-serif',
+            '"Apple Color Emoji"',
+            '"Segoe UI Emoji"',
+            '"Segoe UI Symbol"',
+        ].join(','),
+        h1: {
+            color: theme.palette.primary.main,
+            fontFamily: 'NotoSansHebrew',
+            fontWeight: 400
+        },
+        h2: {
+            // [theme.breakpoints.down("sm")]: {
+            //     fontSize: "3.3rem"
+            // },
+            fontFamily: 'NotoSansHebrew',
+            fontWeight: 500
+        },
+        body1: {
+            fontWeight: 400,
+            fontFamily: 'NotoSansHebrew',
+        }
+    },
+    breakpoints: {
+        values: {
+            xs: 0,
+            sm: 600,
+            md: 900,
+            lg: 1200,
+            xl: 1536,
+        }
+    },
 })
 
 export default theme
--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css"
 import { BrowserRouter as Router } from "react-router-dom";
 import { RootCmp } from "./root-cmp";
-import "./styles/main.scss";
 import SiteProvider from "./store/context";
+import { ThemeProvider } from "@mui/material";
+import theme from "./Theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <SiteProvider>
-    <Router>
-      <RootCmp />
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <RootCmp />
+      </Router>
+    </ThemeProvider>
   </SiteProvider>
 );
