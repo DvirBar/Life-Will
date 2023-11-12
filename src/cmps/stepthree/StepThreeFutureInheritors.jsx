d393ca97b5bd50379100e272293eeff593dc1734
Dvir Bartov <dvir.bartov1@gmail.com>
2023-11-12 03:02:19 +0200

    add remarks

--- src/cmps/stepthree/StepThreeFutureInheritors.jsx
+++ src/cmps/stepthree/StepThreeFutureInheritors.jsx
@@ -4,6 +4,7 @@ import { Typography } from '@mui/material'
 import translation from '../../store/translation'
 import ItemInheritors from '../utils/itemInheritors/ItemInheritors'
 import FormWrapper from '../utils/FormWrapper'
+import FormikTextField from '../formikcomponents/FormikTextField'
 
 function StepThreeFutureInheritors() {
     // TODO: can it be empty?
@@ -18,12 +19,23 @@ function StepThreeFutureInheritors() {
                         <ItemInheritors
                             showTitle={false}
                             name="future_real_estate_data.inheritors" />
+                        <FormikTextField
+                            name="future_real_estate_data.remarks"
+                            label={translation.future_real_estate_data.remarks}
+                            fullWidth
+                            multiline
+                            rows={6}
+                        />
                     </StyledWrapper>
                 )
             }} />
     )
 }
 
-const StyledWrapper = styled.div``
+const StyledWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+`
 
 export default StepThreeFutureInheritors
